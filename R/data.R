1b4090612cfe501206bbf97e1ccbc56772353938
Yufree <yufree@live.cn>
2017-07-27 00:54:50 -0400

    add data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,27 @@
-#' Fish experimental with technique and biological replicates
+#' Two factor fish experimental with technique and biological replicates
 #'
 #' A XCMSnExp object containing 3 days' different treatment with 3 biological replicates, each biological replicate contain 6-8 technique replicates. Use as(fish,"xcmsSet") to convert them to xcmsSet object.
 #'
 #' @format Large XCMSnExp with 996 peaks among 189 samples
 "fish"
+
+#' One factor fish experimental with technique and biological replicates
+#'
+#' A XCMSnExp object containing 1 days' different treatment with 3 biological replicates, each biological replicate contain 6-8 technique replicates. Use as(fish,"xcmsSet") to convert them to xcmsSet object.
+#'
+#' @format Large XCMSnExp with 992 peaks among 63 samples
+"fish2"
+
+#' Fish experimental with technique and biological replicates
+#'
+#' A XCMSnExp object containing 1 days' control samples with 3 biological replicates, each biological replicate contain 6-8 technique replicates. Use as(fish,"xcmsSet") to convert them to xcmsSet object.
+#'
+#' @format Large XCMSnExp with 982 peaks among 20 samples
+"fish3"
+
+#' Fish experimental with biological replicates
+#'
+#' A XCMSnExp object containing 1 days' control sample contain 6-8 technique replicates. Use as(fish,"xcmsSet") to convert them to xcmsSet object.
+#'
+#' @format Large XCMSnExp with 408 peaks among 6 samples
+"fish4"
