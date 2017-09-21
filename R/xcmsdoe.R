62f37718f388e9a699f369901d318c2aaa7d8d75
Yufree <yufree@live.cn>
2017-09-21 12:34:51 -0400

    add update

--- R/xcmsdoe.R
+++ R/xcmsdoe.R
@@ -30,8 +30,10 @@ getbgremove <-
                 suppressWarnings(rsd <- sd / mean * 100)
                 result <-
                         data.frame(cbind(t(mean[,-1]), t(rsd[, -1])))
+                rsd0 <- as.matrix(rsd[,-1])
+                rsd0[is.nan(rsd0)] <- 0
                 index <-
-                        t(rsd[, -1]) < rsdcf & t(mean[, -1]) > 10 ^ (inscf)
+                        t(rsd0) < rsdcf & t(mean[, -1]) > 10 ^ (inscf)
 
                 diff <- result[, 2] - result[, 1]
                 report <-
@@ -92,10 +94,13 @@ gettechrep <-
                 mean <- stats::aggregate(data, list(lv), mean)
                 sd <- stats::aggregate(data, list(lv), sd)
                 suppressWarnings(rsd <- sd / mean * 100)
+
                 result <-
                         data.frame(cbind(t(mean[, -1]), t(sd[, -1]), t(rsd[, -1])))
+                rsd0 <- as.matrix(rsd[,-1])
+                rsd0[is.nan(rsd0)] <- 0
                 index <-
-                        t(rsd[, -1]) < rsdcf & t(mean[, -1]) > 10 ^ (inscf)
+                        t(rsd0) < rsdcf & t(mean[, -1]) > 10 ^ (inscf)
                 colnames(result) <- c("mean", "sd", "rsd")
                 datap <- xcms::groups(xset)
                 report <- cbind.data.frame(datap, result)
@@ -170,7 +175,10 @@ getbiotechrep <-
                 sdB <- apply(t(mean[,-1]), 1, sd)
                 rsdB <- sdB / meanB * 100
 
-                index <- rsdB < rsdcf & meanB > 10 ^ (inscf)
+                rsd0 <- as.matrix(rsdB)
+                rsd0[is.nan(rsd0)] <- 0
+
+                index <- t(rsd0) < rsdcf & meanB > 10 ^ (inscf)
 
                 datap <- xcms::groups(xset)
                 report <-
@@ -243,8 +251,10 @@ getbiorep <-
                           paste0(t(sd[, 1]), "sd"),
                           paste0(t(mean[, 1]), "rsd%"))
 
+                rsd0 <- as.matrix(rsd[,-1])
+                rsd0[is.nan(rsd0)] <- 0
                 indexrsd <-
-                        apply(t(rsd[,-1]), 1, function(x)
+                        apply(t(rsd0), 1, function(x)
                                 any(x < rsdcf))
                 indexmean <-
                         apply(t(mean[,-1]), 1, function(x)
@@ -342,8 +352,10 @@ getgrouprep <-
                 datap <- xcms::groups(xset)
                 report <- cbind.data.frame(datap, result, resultB)
 
+                rsd0 <- as.matrix(rsdB)
+                rsd0[is.nan(rsd0)] <- 0
                 index <-
-                        as.vector(apply(rsdB, 2, function(x)
+                        as.vector(apply(rsd0, 2, function(x)
                                 all(x < rsdcf))) &
                         as.vector(apply(meanB[,-1], 2, function(x)
                                 all(x > 10 ^ (inscf))))
@@ -434,8 +446,10 @@ gettimegrouprep <-
                 datap <- xcms::groups(xset)
                 report <- cbind.data.frame(datap, result, resultB)
 
+                rsd0 <- as.matrix(rsdB)
+                rsd0[is.nan(rsd0)] <- 0
                 index <-
-                        as.vector(apply(rsdB, 2, function(x)
+                        as.vector(apply(rsd0, 2, function(x)
                                 all(x < rsdcf))) &
                         as.vector(apply(meanB[,-c(1, 2)], 2, function(x)
                                 all(x > 10 ^ (inscf))))
@@ -475,4 +489,73 @@ gettimegrouprep <-
                                 return(report)
                         }
                 }
+        }
+
+#' Get the two factor DoE report for samples with biological replicates in different groups
+#' @param xset the xcmsset object all of samples with technique replicates in time series or two factor DoE
+#' @param method parameter for groupval function
+#' @param intensity parameter for groupval function
+#' @param file file name for the peaklist to MetaboAnalyst
+#' @param rsdcf rsd cutoff for peaks, default 30
+#' @param inscf Log intensity cutoff for peaks, default 5
+#' @return dataframe with two factor DoE mean, standard deviation and RSD for those biological replicates combined with raw data in different groups if file are defaults NULL.
+#' @export
+getgroup2rep <-
+        function(xset,
+                 file = NULL,
+                 method = "medret",
+                 intensity = "into",
+                 rsdcf = 30,
+                 inscf = 5) {
+                data0 <- xcms::groupval(xset, method, intensity)
+                data0[is.na(data0)] = 0
+                data <- t(data0)
+                lv <- xset@phenoData[, 1]
+                lv2 <- xset@phenoData[, 2]
+                mean <- stats::aggregate(data, list(lv, lv2), mean)
+                sd <- stats::aggregate(data, list(lv, lv2), sd)
+                suppressWarnings(rsd <- sd / mean * 100)
+
+                result <-
+                        cbind.data.frame(t(mean[, -c(1:2)]), t(sd[, -c(1:2)]), t(rsd[, -c(1:2)]))
+                name <- paste0(t(mean[, 1]), t(mean[, 2]))
+                colnames(result) <-
+                        c(paste0(name, "mean"),
+                          paste0(name, "sd"),
+                          paste0(name, "rsd%"))
+                datap <- xcms::groups(xset)
+                report <- cbind.data.frame(datap, result)
+                rsd0 <- as.matrix(rsd[,-c(1,2)])
+                rsd0[is.nan(rsd0)] <- 0
+                index <-
+                        as.vector(apply(rsd0, 2, function(x)
+                                any(x < rsdcf))) &
+                        as.vector(apply(mean[,-c(1,2)], 2, function(x)
+                                any(x > 10 ^ (inscf))))
+
+                N <- sum(index)
+                L <- length(index)
+
+                message(
+                                paste(
+                                        N,
+                                        'out of',
+                                        L,
+                                        'peaks found with rsd cutoff',
+                                        rsdcf,
+                                        'and Log intensity cutoff',
+                                        inscf
+                                ))
+
+                        report <- report[index,]
+
+                        if (!is.null(file)) {
+                                result <- data.frame(t(mean[, -c(1:2)]))[index,]
+                                data <-
+                                        rbind(group = as.character(mean[, 1]), result)
+                                utils::write.csv(data, file = paste0(file, '.csv'))
+                                return(data)
+                        } else {
+                                return(report)
+                        }
         }
